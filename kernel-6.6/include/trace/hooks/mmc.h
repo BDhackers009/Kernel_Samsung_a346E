0a8e0dc00ccaab8e9479409d429b092017bc6bae
Babak <145232367+BS1388@users.noreply.github.com>
2026-09-13 13:08:52 +0330

    Update mmc.h

--- kernel-6.6/include/trace/hooks/mmc.h
+++ kernel-6.6/include/trace/hooks/mmc.h
@@ -14,8 +14,9 @@ struct sdhci_host;
  * Following tracepoints are not exported in tracefs and provide a
  * mechanism for vendor modules to hook and extend functionality
  */
-#define trace_android_vh_mmc_sdio_pm_flag_set(host) do { (void)(host); } while (0)
-
+DECLARE_HOOK(android_vh_mmc_sdio_pm_flag_set,
+	TP_PROTO(struct mmc_host *host),
+	TP_ARGS(host));
 DECLARE_RESTRICTED_HOOK(android_rvh_mmc_suspend,
 	TP_PROTO(struct mmc_host *host),
 	TP_ARGS(host), 1);
